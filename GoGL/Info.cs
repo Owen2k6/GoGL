5d731c01c246a931f4d881c8ed5e5760485eacd2
Owen2k6 <owen@owen2k6.com>
2024-01-26 22:31:06 +0000

    ae

---
+++ GoGL/Info.cs
@@ -0,0 +1,27 @@
+﻿using GoGL.Graphics;
+using IL2CPU.API.Attribs;
+
+namespace GoGL;
+
+public class Info
+{
+    [ManifestResourceStream(ResourceName = "GoGL.Resources.logo.bmp")]
+    static byte[] GoGLLogoRaw;
+    public Canvas GoGLLogo;
+
+    public String getVersion()
+    {
+        return "0.0.2";
+    }
+
+    public String getApiVersion()
+    {
+        return "1.3";
+    }
+
+    public Canvas getLogo()
+    {
+        GoGLLogo = Image.FromBitmap(GoGLLogoRaw, false);
+        return GoGLLogo;
+    }
+}
