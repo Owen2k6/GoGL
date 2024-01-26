6c95f2556b32da1b649852b22b73a8755bdb3a71
Owen2k6 <owen@owen2k6.com>
2024-01-26 21:37:03 +0000

    relocation cus its cool

---
+++ GoGL/Tools/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace GoGL.Tools;
+
+public static class Coordinates
+{
+	public static float Scale(float Value, float NewSize, float OldSize)
+	{
+		return NewSize * (Value / OldSize);
+	}
+}
---
+++ GoGL/info.cs
@@ -0,0 +1,13 @@
+﻿namespace GoGL;
+
+public class info
+{
+    public String getVersion()
+    {
+        return "0.0.1";
+    }
+    public String getApiVersion()
+    {
+        return "1.3";
+    }
+}
